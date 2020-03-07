9fafe0be465940228b7763afddc1e0c7096c134d
koenig-hub <4stagioni@gmail.com>
2020-03-07 14:50:40 +0100

    Kommentar hinzugefügt

--- projekt03_love-o-meter/sketch_projekt03.ino
+++ projekt03_love-o-meter/sketch_projekt03.ino
@@ -4,7 +4,7 @@
   Love-o-meter
 
   erstellt am 22. Februar 2020
-  von E. König
+  von koenig-hub
 
 */
 
@@ -53,17 +53,17 @@ void loop() {
     digitalWrite(2,LOW);
     digitalWrite(3,LOW);
     digitalWrite(4,LOW);   
-  // ist die gemessene Temperatur zwischen 2 und 4 Grad  höher die Ausgangstemperatur -> LED an Pin 2 einschalten
+  // ist die gemessene Temperatur zwischen 2 und 4 Grad höher als die Ausgangstemperatur -> LED an Pin 2 einschalten
   } else if((temperatur >= ausgangsTemperatur+2) && (temperatur < ausgangsTemperatur+4)) {
     digitalWrite(2,HIGH);
     digitalWrite(3,LOW);
     digitalWrite(4,LOW);     
-  // ist die gemessene Temperatur zwischen 4 und 6 Grad  höher die Ausgangstemperatur -> LEDs an Pin 2 und 3 einschalten
+  // ist die gemessene Temperatur zwischen 4 und 6 Grad höher als die Ausgangstemperatur -> LEDs an Pin 2 und 3 einschalten
   } else if((temperatur >= ausgangsTemperatur+4) && (temperatur < ausgangsTemperatur+6)) {
     digitalWrite(2,HIGH);
     digitalWrite(3,HIGH);
     digitalWrite(4,LOW);     
-  // ist die gemessene Temperatur zwischen 6 und 8 Grad  höher die Ausgangstemperatur -> LEDs an Pin 2, 3 und 4 einschalten
+  // ist die gemessene Temperatur zwischen 6 und 8 Grad höher als die Ausgangstemperatur -> LEDs an Pin 2, 3 und 4 einschalten
   } else if((temperatur >= ausgangsTemperatur+6) && (temperatur < ausgangsTemperatur+8)) {
     digitalWrite(2,HIGH);
     digitalWrite(3,HIGH);
